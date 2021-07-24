ff83570490da585a0a2394444265afbbf807d021
rahul-1810 <74167989+rahul-1810@users.noreply.github.com>
2021-07-24 19:04:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "ap-south-1"
-    AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY }}
-    AWS_SECRET_KEY_ID: ${{ secrets.AWS_SECRET_KEY }}
+    AWS_ACCESS_KEY_ID = ${{ secrets.AWS_ACCESS_KEY }}
+    AWS_SECRET_KEY_ID = ${{ secrets.AWS_SECRET_KEY }}
 }
 
 resource "aws_instance" "os" {
