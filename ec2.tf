a8e33f3bf78543f369485fe308ea088ebfa81f11
rahul-1810 <74167989+rahul-1810@users.noreply.github.com>
2021-07-24 18:58:55 +0530

    second commit

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "os" {
+  ami           = "ami-0ad704c126371a549"
+  instance_type = "t2.micro"
+  key_name = "newkey"
+ tags = {
+    Name = "OS-from-GH"	
+  }
+}
+
